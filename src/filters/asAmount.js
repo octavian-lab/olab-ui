69b720cd897fcf1a5dcb6b38bd5134e456626e09
Davidz99 <davide.crespina@yahoo.it>
2023-11-28 16:50:14 +0100

    versione inserita lato bitbucket, NON VA PIU' MODIFICATA DAL Progetto ci pensa il server

--- src/filters/asAmount.js
+++ src/filters/asAmount.js
@@ -1,30 +1,33 @@
-export default function (value, currency = "EUR") {
-    let precision;
-    switch (currency) {
-        case "EUR":
-            precision = 2;
-            break;
-        case "RUB":
-            precision = 0;
-            break;
-        default:
-            precision = 2;
-            break;
-    }
+export default function (value, currency = 'EUR', hideCurrency = false) {
+  let precision
+  switch (currency) {
+    case 'EUR':
+      precision = 2
+      break
+    case 'RUB':
+      precision = 0
+      break
+    default:
+      precision = 2
+      break
+  }
 
-    if (value == null) return "";
-    let valueDouble;
-    let formatter;
+  if (value == null) return ''
+  let valueDouble
+  let formatter
 
-    switch (currency) {
-        case "PTS":
-            return `${value} ${currency}`;
-        default:
-            formatter = new Intl.NumberFormat("it-IT", {
-                style: "currency",
-                currency: currency,
-            });
-            valueDouble = parseFloat(value) / Math.pow(10, precision);
-            return formatter.format(valueDouble.toFixed(precision));
-    }
+  switch (currency) {
+    case 'PTS':
+      return `${value} ${currency}`
+    default:
+      formatter = new Intl.NumberFormat('it-IT', {
+        style: 'currency',
+        currency: currency
+      })
+      valueDouble = parseFloat(value) / Math.pow(10, precision)
+
+      if (hideCurrency === true) return valueDouble.toFixed(precision)
+
+      return formatter.format(valueDouble.toFixed(precision))
+  }
 }
