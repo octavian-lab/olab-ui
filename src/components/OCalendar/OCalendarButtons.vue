6e8a8d129bb6ffef1258c74154e63fa0c6493c57
Simons99 <simo.angelini11@gmail.com>
2024-01-29 08:40:07 +0100

    agg

---
+++ src/components/OCalendar/OCalendarButtons.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="o-calendar-buttons" class="flex flex-column">
+    <Button icon="fad fa-angle-up" class="border-noround px-2" @click="$emit('onAddDay',1)"/>
+    <Button icon="fad fa-angle-down" class="border-noround px-2"  @click="$emit('onAddDay',-1)"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OCalendarButtons"
+}
+</script>
