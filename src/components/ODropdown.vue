c71b281cf79d73498c28c69f41a076b968ef7a0c
c.antir <Octavian@22>
2024-02-05 18:34:45 +0100

    [odropdown]

--- src/components/ODropdown.vue
+++ src/components/ODropdown.vue
@@ -14,28 +14,26 @@ export default {
     translator: { type: String, default: () => null },
     prependValueOnLabel: { type: Boolean, default: () => true },
     showClear: { type: Boolean, default: () => true },
-    options: { type: Array, required: true }
+    options: { type: [Array, String], required: true }
   },
   data() {
     return {
-      results: this.options
+      results: Array.isArray(this.options) ? this.options : []
     }
   },
   watch: {
     options: {
       immediate: true,
       handler(val) {
-        this.results = this.elaborate(val, this.translator, this.prependValueOnLabel)
+        if (Array.isArray(val)) {
+          this.results = this.elaborate(val, this.translator, this.prependValueOnLabel)
+        }
       }
     }
   },
   methods: {
     elaborate(data, translatePrefix, prependValueOnLabel = true) {
       let ret = []
-      if (!Array.isArray(data)) {
-        console.warn('Traduzione impossibile non è un array o è un array vuoto: ', ret)
-        return ret
-      }
       if (data.length === 0) return ret
       let behaviourSimple = true
       if (typeof data[0] === 'object') {
@@ -58,6 +56,33 @@ export default {
         ret.push({ label, value })
       }
       return ret
+    },
+    getOptionsList() {
+      let ret = []
+      switch (this.options) {
+        case 'skins':
+          ret = [...this.$store.getters.skins]
+          break
+        case 'licensees':
+          ret = [...this.$store.getters.licensees]
+          break
+        case 'platforms':
+          if (this.$store.getters.info.idLicensee) {
+            ret = [...this.$store.getters.platformsByLicensee]
+          } else {
+            ret = [...this.$store.getters.platforms]
+          }
+          break
+        case 'languages':
+          ret = [...this.$store.getters.languages]
+          break
+      }
+      this.results = this.generateSelects(ret, 'id', 'description')
+    }
+  },
+  mounted() {
+    if (typeof this.options === 'string') {
+      this.getOptionsList()
     }
   }
 }
