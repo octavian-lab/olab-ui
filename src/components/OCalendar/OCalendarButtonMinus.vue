beaa0c337cb5138236d23eda52790e619532a96f
Alessio Pomponi <a.pomponi@octavianlab.com>
2024-04-05 18:15:40 +0200

    [Calendar-buttons]

---
+++ src/components/OCalendar/OCalendarButtonMinus.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="o-calendar-button-minus" class="flex flex-column">
+    <Button
+      icon="far fa-minus"
+      :class="[
+        'border-noround px-2',
+        { 'border-top-left-rounded': borderLeftRounded },
+        { 'border-bottom-left-rounded': borderLeftRounded }
+      ]"
+      @click="$emit('onAddDay', -1)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OCalendarButtonMinus',
+  props: {
+    borderLeftRounded: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#o-calendar-button-minus {
+  .border-top-left-rounded {
+    border-top-right-radius: var(--border-radius) !important;
+  }
+  .border-bottom-left-rounded {
+    border-top-right-radius: var(--border-radius) !important;
+  }
+}
+</style>
