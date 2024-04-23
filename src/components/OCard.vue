7e3b1f6e4867aeb31703973e4b9b789d9800e462
m.braccini <m.braccini@octavianlab.com>
2024-04-23 17:39:44 +0200

    OCard rework

--- src/components/OCard.vue
+++ src/components/OCard.vue
@@ -1,33 +1,29 @@
 <template>
-  <Card ref="OCardRef" id="o-card" :class="{ 'h-full': fullHeight }">
+  <Card id="o-card" :class="[{ 'h-full': fullHeight }, getCardClass()]">
     <template v-if="data.title" #title>
-      <div ref="OCardTitleRef" class="o-card__title">
-        <span ref="OCardTitleContainerRef" class="o-card__title-container">
-          <span
-            v-if="styleAdvanced"
-            ref="OCardStyleAdvancedRef"
-            class="o-card__style-advanced"
-          ></span>
+      <div :class="['o-card__title', getTitleClass()]">
+        <span
+          :class="[
+            'o-card__title-container text-special-color',
+            { 'bg-special-color': type === 3 }
+          ]"
+        >
+          <span v-if="type === 3" class="o-card__title-fx"></span>
 
-          <i v-if="mode === 'list' && data.icon" :class="`fad ${data.icon} mr-2`" />
-          <span ref="OCardStyleAdvancedTitleRef">{{
-            $translate(`${data.title}`).includes('--') ? data.title : $translate(`${data.title}`)
-          }}</span>
+          <i v-if="data.icon" :class="`fad ${data.icon} mr-2`" />
+          <span>{{ $translate(data.title) }}</span>
         </span>
       </div>
     </template>
+
     <template #content>
       <div class="o-card__content">
         <slot name="content">
-          <OList :items="data.items" :striped="striped" class="mt-2" />
+          <OList :items="data.items" :striped="striped" :class="{ 'mt-2': data.title }" />
         </slot>
-        <i
-          v-if="mode === 'default' && data.icon"
-          ref="OCardIconModeDefaultRef"
-          :class="`fad ${data.icon} icon__mode-default`"
-        />
       </div>
     </template>
+
     <template v-if="$slots.footer !== undefined" #footer>
       <div class="o-card__footer">
         <slot name="footer" />
@@ -37,57 +33,71 @@
 </template>
 
 <script>
-import { nextTick } from 'vue'
-
 export default {
   name: 'OCard',
   props: {
-    mode: { type: String, default: () => 'list' }, // default, list
     data: { type: Object, required: true },
+    type: { type: Number, required: true },
+    color: { type: Number, required: true },
+    coloredTitle: { type: Boolean, default: () => true },
     fullHeight: { type: Boolean, default: () => true },
-    striped: { Type: Boolean, default: () => true }, // OList only
-    styleAdvanced: { Type: Boolean, default: () => false },
-    // TUTTI I COLOR vanno da 1 a 9 (cdn File)
-    borderColor: { Type: Number },
-    titleColor: { Type: Number },
-    contentColor: { Type: Number },
-    textColor: { Type: Number }
+    striped: { type: Boolean, default: () => true }
   },
-  mounted() {
-    nextTick(() => {
-      if (this.borderColor != null)
-        this.$refs.OCardRef.$el.style.borderLeft = `0.375rem solid var(--special-color-${this.borderColor})`
-      if (this.titleColor != null) {
-        if (this.styleAdvanced) {
-          this.$refs.OCardTitleRef.style.background = `linear-gradient(180deg, var(--special-color-${this.titleColor}) 50%, var(--surface-50) 50%)`
-          this.$refs.OCardTitleContainerRef.style.backgroundColor = `var(--special-color-${this.titleColor})`
-          this.$refs.OCardStyleAdvancedRef.style.backgroundColor = `var(--special-color-${this.titleColor})`
-          this.$refs.OCardStyleAdvancedTitleRef.style.color = `var(--special-color-${this.titleColor}-darken)`
-        } else {
-          if (this.data.title) {
-            this.$refs.OCardTitleRef.style.backgroundColor = `var(--special-color-${this.titleColor})`
-            this.$refs.OCardTitleRef.style.color = `var(--special-color-${this.titleColor}-darken)`
-          }
-        }
-        if (this.data.icon)
-          this.$refs.OCardIconModeDefaultRef.style.color = `var(--special-color-${this.titleColor})`
+  data() {
+    return {
+      colorRef: `var(--special-color-${this.color})`,
+      titleColorRef: this.coloredTitle ? `var(--special-color-${this.color}-darken)` : '#495057'
+    }
+  },
+  methods: {
+    getCardClass() {
+      if (this.color === 0) return ''
+
+      switch (this.type) {
+        case 1:
+        case 2:
+        case 4:
+          return 'border-special-color'
+        case 3:
+          return 'bg-special-color'
       }
-      if (this.contentColor != null)
-        this.$refs.OCardRef.$el.style.backgroundColor = `var(--special-color-${this.contentColor})`
-      if (this.textColor != null) {
-        this.$refs.OCardRef.$el.style.color = `var(--special-color-${this.textColor}-darken)`
-        if (this.data.icon) this.$refs.OCardIconModeDefaultRef.style.opacity = '0.5'
+    },
+    getTitleClass() {
+      if (this.color === 0) return ''
+
+      switch (this.type) {
+        case 1:
+        case 3:
+          return 'bg-special-color'
+        case 2:
+          return ''
+        case 4:
+          return 'bg-special-color-advanced'
       }
-    })
+    }
   }
 }
 </script>
 
 <style lang="scss">
 #o-card {
+  &.border-special-color {
+    border-left: 0.375rem solid v-bind(colorRef);
+  }
+  &.bg-special-color {
+    background-color: v-bind(colorRef);
+  }
+  & .bg-special-color {
+    background-color: v-bind(colorRef);
+  }
+  & .bg-special-color-advanced {
+    background: linear-gradient(180deg, v-bind(colorRef) 50%, transparent 50%);
+  }
+  & .text-special-color {
+    color: v-bind(titleColorRef);
+  }
+
   &.p-card {
-    position: relative;
-    overflow: hidden;
     padding: 0;
     .p-card-body {
       padding: 0;
@@ -103,12 +113,6 @@ export default {
     }
     .p-card-content {
       padding: 0;
-      .icon__mode-default {
-        position: absolute;
-        bottom: -1rem;
-        right: -1.5rem;
-        font-size: 6rem;
-      }
     }
     .p-card-footer {
       padding: 1rem;
@@ -120,41 +124,45 @@ export default {
       border-radius: 0 0 0.75rem 0.75rem;
       position: relative;
     }
-    .o-card__title-container i,
-    .o-card__title-container span:not(.o-card__style-advanced) {
-      position: relative;
-      z-index: 3;
-    }
-    .o-card__style-advanced {
+    .o-card__title-fx {
       width: calc(100% + 0.75rem + 0.75rem);
-      height: calc(0.75rem + 0.75rem);
       position: absolute;
       top: 50%;
       left: -0.75rem;
       transform: translateY(-50%);
       z-index: 1;
     }
-    .o-card__style-advanced::before {
+    .o-card__title-fx::before {
       content: '';
       width: calc(0.75rem + 0.75rem);
       aspect-ratio: 1;
-      background-color: var(--surface-50);
-      border-radius: 0 50% 0 0;
       position: absolute;
-      top: 0.75rem;
+      top: 0;
       left: -0.75rem;
       z-index: 2;
+      background: radial-gradient(
+        circle,
+        transparent calc((100% / 3) * 2),
+        v-bind(colorRef) calc((100% / 3) * 2),
+        v-bind(colorRef) 100%
+      );
+      clip-path: polygon(50% 0, 100% 0, 100% 50%, 50% 50%);
     }
-    .o-card__style-advanced::after {
+    .o-card__title-fx::after {
       content: '';
       width: calc(0.75rem + 0.75rem);
       aspect-ratio: 1;
-      background-color: var(--surface-50);
-      border-radius: 50% 0 0 0;
       position: absolute;
-      top: 0.75rem;
+      top: 0;
       right: -0.75rem;
       z-index: 2;
+      background: radial-gradient(
+        circle,
+        transparent calc((100% / 3) * 2),
+        v-bind(colorRef) calc((100% / 3) * 2),
+        v-bind(colorRef) 100%
+      );
+      clip-path: polygon(50% 0, 0 0, 0 50%, 50% 50%);
     }
   }
 }
