abe7731935857fe07a4b132cb244f7fff460a9f3
c.antir <Octavian@22>
2024-05-02 11:52:28 +0200

    OBannerBrowserSupport

--- src/components/OBannerBrowserSupport.vue
+++ src/components/OBannerBrowserSupport.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="banner-browser-support" class="zIndex font-bold fixed top-0 left-0 right-0">
     <div
-      v-if="!isSupported"
+      v-if="!showBanner"
       class="p-3 h-5rem bg-red-500 font-bold flex flex-row justify-content-between align-items-center"
     >
       <div class="flex align-items-center">
@@ -9,8 +9,18 @@
           <i class="fad fa-triangle-exclamation text-xl mr-1"></i
           >{{ $translate('admin.generic.browser.not.supported') }}
         </div>
-        <img class="h-2rem w-2rem ml-2" src="../assets/images/chrome.svg" alt="" />
-        <img class="h-2rem w-2rem ml-2" src="../assets/images/firefox.svg" alt="" />
+        <img
+          class="h-2rem w-2rem ml-3"
+          src="../assets/images/chrome.svg"
+          alt=""
+          @click="openLink(link.chrome)"
+        />
+        <img
+          class="h-2rem w-2rem ml-3"
+          src="../assets/images/firefox.svg"
+          alt=""
+          @click="openLink(link.firefox)"
+        />
       </div>
       <Button class="text-2xl text-white ml-2" text icon="fa-solid fa-xmark" @click="closeBanner" />
     </div>
@@ -25,7 +35,7 @@ export default {
     return {
       supportedBrowsers: ['Chrome', 'Firefox'],
       parser: new UAParser(),
-      isSupported: true,
+      showBanner: null,
       link: {
         chrome: 'https://www.google.com/intl/it_it/chrome/',
         firefox: 'https://www.mozilla.org/it/firefox/new/'
@@ -34,14 +44,14 @@ export default {
   },
   methods: {
     closeBanner() {
-      this.isSupported = false
+      this.showBanner = true
     },
     openLink(link) {
       window.open(link, '_blank')
     },
     checkBrowserSupport() {
       const browser = this.parser.getBrowser().name
-      this.isSupported = this.supportedBrowsers.some((el) => {
+      this.showBanner = this.supportedBrowsers.some((el) => {
         return el.includes(browser)
       })
     }
@@ -54,5 +64,9 @@ export default {
 <style lang="scss" scoped>
 .zIndex {
   z-index: 2000;
+}
+
+img {
+  cursor: pointer;
 }
 </style>
