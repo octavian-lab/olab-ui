8444206acb6d2f7789988043d6cab8c57985d24b
Simons99 <simo.angelini11@gmail.com>
2024-09-02 11:24:46 +0200

    timezone handler added

--- src/filters/asDate.js
+++ src/filters/asDate.js
@@ -1,8 +1,14 @@
-import moment from 'moment'
-
+import moment from 'moment-timezone'
+const site = localStorage.getItem('site')
+const utils = localStorage.getItem(`olab-ui-${site}:utils`) ? JSON.parse(localStorage.getItem(`olab-ui-${site}:utils`)) : {}
+const timezone = utils.timezone
 export default function (value, format = 'DD/MM/YYYY HH:mm:ss') {
   if (value) {
     const utcDate = moment.utc(value).toDate()
+    if (timezone && timezone !== moment.tz.guess()) {
+      const ret = moment.tz(utcDate, timezone).format(format);
+      return ret
+    }
     const localMoment = moment(utcDate).local()
       localMoment.format(format);
       return localMoment.format(format)
